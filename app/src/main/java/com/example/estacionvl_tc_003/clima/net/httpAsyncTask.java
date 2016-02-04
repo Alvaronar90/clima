e5019f9c3e47cc20c43cd01a30174bedf190a3fb
Duvan Muñoz <damj_24@hotmail.com>
2016-02-03 19:59:36 -0500

    Clase coneccion

---
+++ app/src/main/java/com/example/estacionvl_tc_003/clima/net/httpAsyncTask.java
@@ -0,0 +1,20 @@
+package com.example.estacionvl_tc_003.clima.net;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by EstacionVL-TC-003 on 3/02/16.
+ */
+public class httpAsyncTask extends AsyncTask<String, Integer, String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+    }
+}
